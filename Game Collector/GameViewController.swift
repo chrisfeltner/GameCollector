e31149586926b3e3d58ea43abf40bedaefab4341
Christopher Feltner <chris.feltner@knights.ucf.edu>
2017-02-28 13:01:06 -0500

    built storyboard

---
+++ Game Collector/GameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  Game Collector
+//
+//  Created by Christopher Feltner on 2/28/17.
+//  Copyright © 2017 Christopher Feltner. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onCameraPressed(_ sender: Any) {
+    }
+
+    @IBAction func onPhotosPressed(_ sender: Any) {
+    }
+
+    
+    @IBAction func onAddPressed(_ sender: Any) {
+    }
+
+
+}
